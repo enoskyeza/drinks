a2b14e7f934c82595725bda51c6f0d7bc496bcbd
enoskyeza <enoskyeza@gmail.com>
2023-02-14 16:09:44 +0300

    added other request methods

--- drinks/views.py
+++ drinks/views.py
@@ -1,14 +1,49 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializer import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-def drink_list(request):
+
+@api_view(['GET', 'POST'])
+def drink_list(request, format=None):
     
     #get all drinks
     #serialize them 
     #return jason list of the drinks
 
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
 
-    return JsonResponse({'drinks' : serializer.data})
+        return Response({'drinks' : serializer.data})
+    
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_details(request, id, format=None):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET': 
+        serializer = DrinkSerializer(drink)
+        return Response({'drink' : serializer.data})
+
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
